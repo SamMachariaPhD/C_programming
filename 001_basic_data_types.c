dfe5d4f439534f22a0e35a5e6d6487f65cf019fe
sirmaxford <sirmaxford@gmail.com>
2021-03-18 02:42:47 +0900

    Initial commit for this repo

---
+++ 001_basic_data_types.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main ()
+{
+    int num_rows, num_cols;
+    num_rows = 10; num_cols = 30;
+    printf("%d\n",num_rows*num_cols);
+    return(0);
+}
---
+++ hello.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main()
+{
+    printf("I'm Sam. \nThis's C. \nHow nice :)\n");
+    return(0);
+}
